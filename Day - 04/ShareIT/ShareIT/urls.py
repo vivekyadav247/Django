e843b529b4a0475c0b77058ca77bbea151b0c816
Vivek Yadav <vivekyad240706@gmail.com>
2025-05-24 11:58:23 +0530

    Theme Integrated

--- Day - 04/ShareIT/ShareIT/urls.py
+++ Day - 04/ShareIT/ShareIT/urls.py
@@ -6,5 +6,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('',views.home),
+    path('about/',views.about),
+    path('contact/',views.contact),
+    path('service/',views.service),
+    path('register/',views.register),
+    path('login/',views.login)
 ]
--- Day - 04/ShareIT/ShareIT/views.py
+++ Day - 04/ShareIT/ShareIT/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"home.html")
+def about(request):
+    return render(request,"about.html")
+def contact(request):
+    return render(request,"contact.html")
+def service(request):
+    return render(request,"service.html")
+def login(request):
+    return render(request,"login.html")
+def register(request):
+    return render(request,"register.html")
